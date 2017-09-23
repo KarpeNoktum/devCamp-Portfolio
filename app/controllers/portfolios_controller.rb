84793a6c6f56c56442af1e596a6a80f7303510d1
Josh Beere <officialkarpenoktum@gmail.com>
2017-09-23 11:07:14 -0400

    Minor change

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,6 +4,26 @@ def index
 	@portfolio_items = Portfolio.all
 end
 
+def new
+	@portfolio_item = Portfolio.new
+
+end
+
+
+
+
+def create
+    @portfolio_item = Portfolio.new( params.require(:portfolio).permit(:title, :body, :subtitle))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Potfolio item was successfully created.' } 
+      else
+        format.html { render :new }
+      end
+    end
+ end
+
 
 
 end
