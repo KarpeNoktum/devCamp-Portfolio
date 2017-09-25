e18d1849103d236f94e3eaac6937c27e637c9244
Josh Beere <officialkarpenoktum@gmail.com>
2017-09-25 14:09:37 -0400

    '

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,6 +43,17 @@ def show
   @portfolio_item = Portfolio.find(params[:id])
 end
 
+def destroy
+@portfolio_item = Portfolio.find(params[:id])
+
+@portfolio_item.destroy
+
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully removed.' }
+    end
+end
+
 
 end
 
