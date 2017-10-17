0ef5920b71696605ab6618ac80ea19a12b2dfa49
Josh Beere <officialkarpenoktum@gmail.com>
2017-10-17 13:49:02 -0400

    Implemented default data values for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,15 @@ def self.angular
 end
 
 scope :ruby_on_rails_portfolio_items, -> {where(subtitle: "Ruby on Rails")}
+
+after_initialize :set_defaults
+
+def set_defaults
+	self.title ||= "[Default Title]"
+	self.subtitle ||= "[Default Subtitle]"
+	self.body ||= "[Default Body]"
+	self.main_image ||= "http://via.placeholder.com/600x400"
+	self.thumb_image ||= "http://via.placeholder.com/350x200"
+end
+
 end
