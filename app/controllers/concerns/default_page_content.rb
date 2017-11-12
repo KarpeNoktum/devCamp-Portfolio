79b0057a2cb303efb41d1c81691d0116283becbf
Josh Beere <officialkarpenoktum@gmail.com>
2017-11-12 12:59:56 -0500

    Implemented layouts and partails for view files

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -9,6 +9,7 @@ module DefaultPageContent
 	def set_page_defaults
 		@page_title = "Josh Beere | Portfolio"
 		@seo_keywords = "Josh Beere Portfolio"
+		@icon = "https://upload.wikimedia.org/wikipedia/commons/1/16/Ruby_on_Rails-logo.png"
 		
 	end
 
