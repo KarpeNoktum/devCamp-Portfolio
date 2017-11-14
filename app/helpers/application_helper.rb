cd5a72bed9a6a88e8dd425a61682c0112794460d
Josh Beere <officialkarpenoktum@gmail.com>
2017-11-14 11:53:27 -0500

    Implemented view helpers for login buttons and session message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+	def login_helper
+		if current_user.is_a?(User)
+       link_to "Sign Out", destroy_user_session_path, method: :delete
+       else
+       	(link_to "Sign In", new_user_session_path) + " | " +
+       	(link_to "Sign Up", new_user_registration_path)
+        end
+	end
+
+	def session_msg_helper
+
+	if session[:source]
+    "<p>Thanks for visiting me from #{session[:source]}!</p>".html_safe
+    end
+
+	end
 end
