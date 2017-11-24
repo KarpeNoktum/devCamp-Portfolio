7a2e3468ccbc02095e459e0f2d6732077a79f1bd
Josh Beere <officialkarpenoktum@gmail.com>
2017-11-24 00:05:28 -0500

    Implemented a navbar for application layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,16 @@ class PagesController < ApplicationController
   	@posts = Blog.all
   	@skills = Skill.all
   	@page_title = "Josh Beere | Home"
+    @home = 'nav-link active'
   end
 
   def about
+    @about = 'nav-link active'
   	@page_title = "Josh Beere | About"
   end
 
   def contact
+    @contact = 'nav-link active'
   	@page_title = "Josh Beere | Contact"
   end
 end
